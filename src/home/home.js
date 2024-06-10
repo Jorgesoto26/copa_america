2bbd821db351a005c72783ece22e916d5f1e24a9
OSCAR OROZCO <oscarorozco@MacBook-Pro-de-OSCAR.local>
2024-06-10 10:26:08 -0500

    login copa ameria y registro

--- src/home/home.js
+++ src/home/home.js
@@ -1,43 +1,127 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
-import { Button, Navbar, Container, Row, Col, Image } from 'react-bootstrap';
-import logo from '../assets/App-logo.png'
+import './home.css'; 
+import Img from '../assets/App-logo2.png';
+import { Image } from 'react-bootstrap'; 
 
 const Home = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <nav>
-                    <ul>
-                        <li>Inicio</li>
-                        <li>Participar</li>
-                        <li>Resultados</li>
-                        <li>Contacto</li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <section className="banner">
-                    {/* <img src="/banner-copa-america.png" alt="Copa América 2024" style={{width: '20%'}} /> */}
-                    <Image src={logo}
-                      style={{ width: 280, height: 'auto', marginBottom: '0%', marginTop: '2%' }}
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [cedula, setCedula] = useState('');
+
+  const handleRegisterClick = () => {
+    setShowRegisterForm(true);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleCedulaChange = (e) => {
+    setCedula(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validar que las contraseñas coincidan
+    if (password === confirmPassword) {
+      // Las contraseñas coinciden, puedes continuar con el registro
+      console.log('Contraseñas coinciden. Puedes continuar con el registro.');
+      console.log('Cédula:', cedula);
+    } else {
+      // Las contraseñas no coinciden, muestra un mensaje de error
+      console.log('Las contraseñas no coinciden. Por favor, intenta de nuevo.');
+      setPasswordsMatch(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <main>
+        <section className="banner"></section>
+        <div className="wrapper">
+          {!showRegisterForm ? (
+            <div className="login-wrapper"> {/* Wrapper para iniciar sesión */}
+              <form action="#">
+                <Image src={Img} />
+                <h2>Iniciar sesión</h2>
+                <div className="input-field">
+                  <input type="text" required />
+                  <label>Ingrese su correo</label>
+                </div>
+                <div className="input-field">
+                  <input type="password" required />
+                  <label>Ingrese la contraseña</label>
+                </div>
+                <button type="submit">Iniciar sesión</button>
+                <div className="register">
+                  <p>
+                    ¿No tienes una cuenta?{' '}
+                    <a href="#" onClick={handleRegisterClick}>
+                      Registrarse
+                    </a>
+                  </p>
+                </div>
+              </form>
+            </div>
+          ) : (
+            <div className="register-wrapper"> {/* Wrapper para registro */}
+              <form onSubmit={handleSubmit}>
+                <Image src={Img} />
+                <h2>Registrarse</h2>
+                <div className="input-field">
+                  <input type="text" required />
+                  <label>Ingrese su nombre</label>
+                </div>
+                <div className="input-field">
+                  <input type="email" required />
+                  <label>Ingrese su correo</label>
+                </div>
+                <div className="input-field">
+                  <input
+                    type="password"
+                    required
+                    value={password}
+                    onChange={handlePasswordChange}
                   />
-                    <h1>Participa en el Sorteo de la Copa América 2024</h1>
-                    <button className="btn-primary">Participar Ahora</button>
-                </section>
-                <section className="participation-form">
-                    <h2>Ingresa tu Código de Compra</h2>
-                    <form>
-                        <input type="text" placeholder="Código de Compra" />
-                        <button type="submit" className="btn-submit">Enviar</button>
-                    </form>
-                </section>
-            </main>
-            <footer>
-                <p>&copy; 2024 Centro Comercial Mall Plaza. Todos los derechos reservados.</p>
-            </footer>
+                  <label>Ingrese la contraseña</label>
+                </div>
+                <div className="input-field">
+                  <input
+                    type="password"
+                    required
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                  />
+                  <label>Confirmar contraseña</label>
+                  {!passwordsMatch && <p className="error-message">Las contraseñas no coinciden.</p>}
+                </div>
+                <div className="input-field">
+                  <input
+                    type="text"
+                    required
+                    value={cedula}
+                    onChange={handleCedulaChange}
+                  />
+                  <label>Ingrese su cédula</label>
+                </div>
+                <button type="submit">Registrarse</button>
+              </form>
+            </div>
+          )}
         </div>
-    )
+      </main>
+      <footer>
+        <p>&copy; 2024 Centro Comercial Mall Plaza. Todos los derechos reservados.</p>
+      </footer>
+    </div>
+  );
 };
+
 export default Home;
